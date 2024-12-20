ba7966f8b8aecf822e12c18db916cf9315fa0f02
Mohammad Bahadori <Mohammad.Bahadori@des.qld.gov.au>
2024-12-20 17:18:21 +1000

    Created SoilData set, the EC, removed the o numbers, and created a graph

--- Codes/Code 1.R
+++ Codes/Code 1.R
@@ -1,2 +1,23 @@
 #codes for Paddock Data Processing
-#
+# updoad soil data from .dat file
+soilData <- read.table(file = "Ross T1_SoilData.dat", header = TRUE, sep = "\t")
+
+# After loading you can check the first few rows to make sure it's loaded correctly
+head(soilData)
+
+# You can check the structure of the data
+str(soilData)
+
+# Assuming soilData is already loaded
+# Example: soilData <- read.table(...)
+
+# Create new data frame 'EC' using the column indices
+# 
+EC <- soilData[, c(1, 4, 7, 10, 13, 16, 19)]
+
+# Alternatively, if the columns are consecutive, you can use
+# EC <- soilData[, 1:6]
+
+# Check the structure and head of the 'EC' dataset
+str(EC)
+head(EC)
