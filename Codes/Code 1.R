4303e784d6bb0ec160a7ff2f3f9ab2c8cf95c297
Mohammad Bahadori <Mohammad.Bahadori@des.qld.gov.au>
2024-12-21 18:22:41 +1000

    unnecessary rows were eliminated.

--- Codes/Code 1.R
+++ Codes/Code 1.R
@@ -1,22 +1,30 @@
 #codes for Paddock Soil Data Processing
-# updoad soil data from .dat file
-soilData <- read.table(file = "Ross T1_SoilData.dat", header = TRUE, sep = "\t")
+#updoad soil data from .dat file
 
-# After loading you can check the first few rows to make sure it's loaded correctly
-head(soilData)
+#Read the file without headers
+tempData <- read.table(file = "Ross T1_SoilData.dat", header = FALSE, sep = "\t")
 
-# You can check the structure of the data
-str(soilData)
+# Check that the file has been read correctly by looking at the top
+head(tempData)
+str(tempData)
 
-# Assuming soilData is already loaded
+# Set the column names to the values of the second row
+colnames(tempData) <- as.character(tempData[2, ])
+
+# Remove unnecessary rows 
+SoilData <- tempData[-c(1, 2, 3, 4), ]
+
+# Confirm the changes by looking at the top of the modified dataframe
+head(SoilData)
+str(SoilData)
 
 
 # Create new data frame 'EC' using the column indices
 # 
-EC <- soilData[, c(1, 4, 7, 10, 13, 16, 19)]
+EC <- SoilData[, c(1, 4, 7, 10, 13, 16, 19)]
 
 # Alternatively, if the columns are consecutive, you can use
-# EC <- soilData[, 1:6]
+# EC <- SoilData[, 1:6]
 
 # Check the structure and head of the 'EC' dataset
 str(EC)
@@ -29,27 +37,12 @@ ECp <- EC[!apply(EC == 0, 1, any), ]
 str(ECp)
 head(ECp)
  
-# Write the 'ECp' dataframe to a .dat file with a tab delimiter
+# Write the 'ECp' dataframe to a .dat file with a tab delimiter and save it
 write.table(ECp, file = "ECp.dat", sep = "\t", row.names = FALSE, col.names = TRUE)
 
-# Assuming ECp is your dataframe
-# Remove the 1st, 2nd and 3rd rows from ECp
-ECp <- ECp[-c(1, 2, 3), ]
-
-# Check the top of the dataframe to ensure the rows are removed
-head(ECp)
 
 
-#to create time stamp for 1 column
-
-# Convert the first column from string to POSIXct datetime object
-ECp[, 1] <- as.POSIXct(ECp[, 1], format = "%d/%m/%Y %H:%M")
-
-# Plotting the converted datetime against the second column
-# Replace 'type' with "l" if you want a line plot instead
-plot(ECp[, 1], ECp[, 2], type = 'p',
-     xlab = 'Timestamp', ylab = 'EC1 Value', main = 'EC1 Values Over Time')
 
 
-#to create timestamp for all columns
+
 
