49b4ba8c79d4b8947723508f3c331644dc64bec6
Mohammad Bahadori <Mohammad.Bahadori@des.qld.gov.au>
2024-12-23 16:04:03 +1000

    Made a timestamp buth manual range for y.

--- Codes/Code 1.R
+++ Codes/Code 1.R
@@ -37,8 +37,39 @@ ECp <- EC[!apply(EC == 0, 1, any), ]
 str(ECp)
 head(ECp)
  
-# Write the 'ECp' dataframe to a .dat file with a tab delimiter and save it
+#Write the 'ECp' dataframe to a .dat file with a tab delimiter and save it
 write.table(ECp, file = "ECp.dat", sep = "\t", row.names = FALSE, col.names = TRUE)
+
+
+#Create the plot using ggplot2
+
+#Assuming ECp has been loaded and column 1 is in the expected date format
+
+# Convert the date strings in column 1 to POSIXct datetime objects
+ECp[, 1] <- as.POSIXct(ECp[, 1], format = "%d/%m/%Y %H:%M")
+
+#Choose your desired y-axis limits (and change it manually in line below).
+y_min <- min(c(ECp[, 2], ECp[, 3], ECp[, 4], ECp[, 5], ECp[, 6], ECp[, 7]), na.rm = TRUE)
+y_max <- max(c(ECp[, 2], ECp[, 3], ECp[, 4], ECp[, 5], ECp[, 6], ECp[, 7]), na.rm = TRUE)
+
+#Create a plot with timestamp on the x-axis and adjusted y-axis limits (adjust the y_max and y_min as you wish)
+plot(ECp[, 1], ECp[, 2], type = 'l', col = 'red', xlab = 'Timestamp', ylab = 'Values', 
+     main = 'Values Over Time', ylim = c(0.1, 0.4))
+
+# Add the rest of the series to the same plot
+lines(ECp[, 1], ECp[, 3], col = 'blue')
+lines(ECp[, 1], ECp[, 4], col = 'green')
+lines(ECp[, 1], ECp[, 5], col = 'purple')
+lines(ECp[, 1], ECp[, 6], col = 'orange')
+lines(ECp[, 1], ECp[, 7], col = 'yellow')
+# Add a legend to distinguish the different lines
+legend("topleft", legend = c("Series 2", "Series 3", "Series 4", "Series 5", "Series 6"), 
+       col = c('red', 'blue', 'green', 'purple', 'orange', 'yellow'), lty = 1, cex = 0.8)
+
+
+# make a graph to compare EC across lysimeters
+
+
 
 
 
